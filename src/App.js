596c78068a0247a3e7c5992eb30b48b02bfc33e6
calher99 <calher99@gmail.com>
2023-05-12 10:51:44 +0100

    fix type bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MeetingDetailForm from "./pages/MeetingDetailForm";
-import MeetingTimer from "./pages/MeetingTimer";
+
 import DynamicForm from "./pages/DynamicForm";
 
 
-const router = createBrowserRouter([
-  {path: "/", element: <MeetingDetailForm/>},
-  {path: "/timer", element: <MeetingTimer/>}
-])
 
 function App() {
   
   return (
-    // <RouterProvider router={router}/>
+
     <DynamicForm></DynamicForm>
   
   );
--- src/pages/DynamicForm.js
+++ src/pages/DynamicForm.js
@@ -6,7 +6,7 @@ import {
   IconButton,
   TextField,
 } from "@mui/material";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
 
